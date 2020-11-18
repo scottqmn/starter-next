7aa9dd4800b1f866bc4b6813bea4ffc6e795f964
Scott Nguyen <scottqmn@gmail.com>
2020-11-17 19:31:49 -0800

    Gtag script

--- utils/gtag.js
+++ utils/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "G-RXLF72FMJE";
+export const GA_TRACKING_ID = "G-TT3B09DSDF";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
