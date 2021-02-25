6d81d33e88fe75980b35e0dbba9a79c7db2c8f98
Scott Nguyen <scottqmn@gmail.com>
2021-02-25 09:05:41 -0800

    Update banner color/dynamic overlay

--- components/Slices/Banner/Banner.jsx
+++ components/Slices/Banner/Banner.jsx
@@ -1,14 +1,35 @@
 import clsx from 'clsx'
+import Color from 'color'
 import { NextImage, RichText } from '../../Prismic'
 import styles from './Banner.module.scss'
 import { prismicSliceComponent } from '../../../prop-types/prismic'
 
 const Banner = ({ primary }) => {
-    const { bg_image, label, subtitle, title, overlay } = primary
+    const {
+        bg_image,
+        label,
+        subtitle,
+        title,
+        color = '#000000',
+        overlay,
+    } = primary
+
+    const colorIsLight = Color(color).isLight()
+
     return (
-        <div className={clsx(styles.outer, 'outer')}>
+        <div
+            className={clsx(styles.outer, 'outer')}
+            style={{ '--text': color }}
+        >
             {bg_image && <NextImage image={bg_image} className={styles.bg} />}
-            {overlay && <div className={styles.overlay} />}
+            {overlay && (
+                <div
+                    className={clsx(
+                        styles.overlay,
+                        colorIsLight && styles.isLight
+                    )}
+                />
+            )}
             <div
                 className={clsx(
                     styles.inner,
